6f6f2440c1d125185121359c69ac66666120ad01
AnitaAkter <108604188+AnitaAkter@users.noreply.github.com>
2022-10-27 00:34:18 +0600

    third-commit

--- index.js
+++ index.js
@@ -29,15 +29,3 @@ app.get('/category/:id', (req, res) => {
     const category_details = category.find(n => n._id === id);
     res.send(category_details);
 });
-
-app.get('/checkOut_page/:id', (req, res) => {
-    const id = req.params.id;
-    const category_details = category.find(n => n._id === id);
-    res.send(category_details);
-});
-
-app.listen(Port, () => {
-    console.log("done", Port);
-});
-
-module.exports = app;
